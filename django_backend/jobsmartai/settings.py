c7ac2f903742740dbd019f5a0933bc0ff1cc16c2
lucas <lucas@local>
2025-05-13 14:39:15 +0330

    MEDIA URL added.

--- django_backend/jobsmartai/settings.py
+++ django_backend/jobsmartai/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -130,3 +131,5 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
 }
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- django_backend/jobsmartai/urls.py
+++ django_backend/jobsmartai/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/resumes/', include('resumes.urls')),
-    path('schema', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-]
+urlpatterns = ([
+                   path('admin/', admin.site.urls),
+                   path('api/resumes/', include('resumes.urls')),
+                   path('schema', SpectacularAPIView.as_view(), name='schema'),
+                   path('swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+                   path('redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
