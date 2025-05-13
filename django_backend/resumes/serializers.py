c367b2f5058c1d79b77dd4a5286615f9765a63ab
lucas <lucas@local>
2025-05-13 14:38:53 +0330

    File modified and additional lines removed.

--- django_backend/resumes/serializers.py
+++ django_backend/resumes/serializers.py
@@ -4,12 +4,7 @@ from .models import ResumeModel
 
 
 class ResumeSerializer(serializers.ModelSerializer):
-
-    linkedin_url = serializers.URLField(
-        help_text="LinkedIn profile URL"
-    )
-
     class Meta:
         model = ResumeModel
-        fields = ['resume_file', 'linkedin_url']
+        fields = ['id', 'file', 'linkedin_url']
         read_only_fields = ['created_at', 'updated_at']
