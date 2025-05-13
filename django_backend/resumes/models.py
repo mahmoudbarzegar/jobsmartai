995aa05e0d2e111dbe44c4b85b9668418a633604
lucas <lucas@local>
2025-05-13 14:38:27 +0330

    Field renamed. Delete method override so the file is deleted too.

--- django_backend/resumes/models.py
+++ django_backend/resumes/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 class ResumeModel(models.Model):
-    resume_file = models.FileField(upload_to='resumes/')
+    file = models.FileField(upload_to='resumes/')
     linkedin_url = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Resume {self.id} - {self.linkedin_url}"
+
+    def delete(self, *args, **kwargs):
+        # Delete the file from storage
+        if self.file:
+            self.file.delete(save=False)
+        # Delete the model instance
+        super().delete(*args, **kwargs)
