ad804e26f2c64d8d00b512d06a7dfacf4deb0837
Mahmoud <hedesh.mb@gmail.com>
2025-10-08 15:50:22 +0330

    Button style has changed.

--- streamlit_frontend/app.py
+++ streamlit_frontend/app.py
@@ -15,7 +15,35 @@ styles = {
     "hover": {"background-color": "#ddd", "color": "#000000"},
 }
 
-
+st.markdown("""
+    <style>
+        div.stFormSubmitButton > button {
+            background-color: #3A7CA5;  
+            color: #FFFFFF;          
+        }
+        div.stFormSubmitButton > button:hover {
+              background-color: #D4E4F2;
+              color: #285E82;
+              border-color: #285E82;
+        }
+        div.stForm {
+            border: 3px solid rgba(250, 250, 250, 0.2);
+        }
+         div.stButton > button {
+            background-color: #1E2A36;  
+            color: #A7C7E7;
+            border: 1px solid #3A7CA5;
+            border-radius: 8px;
+            padding: 0.5rem 1rem;
+            transition: all 0.3s ease;      
+        }
+        div.stButton > button:hover {
+            background-color: #285E82;  
+            color: #FFFFFF;   
+            border-color: #285E82;   
+        }
+    </style>
+    """, unsafe_allow_html=True)
 
 with st.sidebar:
     selected = option_menu(
