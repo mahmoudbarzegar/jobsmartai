8a935c3a866ab5d21b879d9ac66776f6fc1968e5
mahmoudbarzegar <hedesh.mb@gmail.com>
2026-09-01 14:08:36 +0330

    The analyze_resume_with_ollama method was updated.

--- django_backend/jobs/views/resume.py
+++ django_backend/jobs/views/resume.py
@@ -1,19 +1,17 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import status, viewsets
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.response import Response
 
+from ..ai_utils import analyze_resume_with_ollama
 from ..models import ResumeModel
 from ..serializers import ResumeSerializer
-from ..ai_utils import analyze_resume_with_ollama
 from ..utils import extract_text_from_pdf
 
 
-@extend_schema(
-    tags=["Resume"]
-)
+@extend_schema(tags=["Resume"])
 class ResumeViewSet(viewsets.ModelViewSet):
-    http_method_names = ['post', 'get', 'delete']
+    http_method_names = ["post", "get", "delete"]
     model_class = ResumeModel
     queryset = model_class.objects.all()
     serializer_class = ResumeSerializer
@@ -23,13 +21,8 @@ class ResumeViewSet(viewsets.ModelViewSet):
         request={
             "multipart/form-data": {
                 "type": "object",
-                "properties": {
-                    "file": {
-                        "type": "string",
-                        "format": "binary"
-                    }
-                },
-                "required": ["file"]
+                "properties": {"file": {"type": "string", "format": "binary"}},
+                "required": ["file"],
             }
         },
     )
@@ -38,26 +31,28 @@ class ResumeViewSet(viewsets.ModelViewSet):
             serializer = self.get_serializer(data=request.data)
             if not serializer.is_valid():
                 return Response(
-                    {'status': 'error', 'error_message': 'Request is not valid', 'errors': serializer.errors},
-                    status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+                    {"status": "error", "error_message": "Request is not valid", "errors": serializer.errors},
+                    status=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                )
 
-            resume_text = extract_text_from_pdf(request.data['file'])
+            resume_text = extract_text_from_pdf(request.data["file"])
             analyze_resume_text = analyze_resume_with_ollama(resume_text)
+            data = analyze_resume_text.model_dump()
 
-            serializer.save(resume_info=analyze_resume_text)
-            return Response({'status': 'success', 'result': serializer.data}, status=status.HTTP_201_CREATED)
+            serializer.save(**data, resume_info_raw=data)
+            return Response({"status": "success", "result": serializer.data}, status=status.HTTP_201_CREATED)
         except Exception as e:
-            return Response({'status': 'error', 'error_message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"status": "error", "error_message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         serializer = self.get_serializer(queryset, many=True)
-        return Response({'status': 'success', 'result': {'data': serializer.data}}, status=status.HTTP_200_OK)
+        return Response({"status": "success", "result": {"data": serializer.data}}, status=status.HTTP_200_OK)
 
     def destroy(self, request, *args, **kwargs):
         try:
             instance = self.get_object()
             instance.delete()
-            return Response({'status': 'success', 'result': 'Resume deleted successfully'}, status=status.HTTP_200_OK)
+            return Response({"status": "success", "result": "Resume deleted successfully"}, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response({'status:': 'error', 'error_message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({"status:": "error", "error_message": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
